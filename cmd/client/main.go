1ee4cfaac2a8cc158b76f0437d585aedeccadcad
ensn <ensn>
2022-08-05 18:27:38 +0800

    refactor: new client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,104 +2,84 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"sync"
-	"time"
 
 	"github.com/ensn1to/tcp_server_demo/frame"
 	"github.com/ensn1to/tcp_server_demo/packet"
+
 	"github.com/lucasepe/codename"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	var num int = 100
-
-	wg.Add(num)
-	for i := 0; i < num; i++ {
-		go func(i int) {
-			defer wg.Done()
-			startClient(i)
-		}(i + 1)
-	}
-	wg.Wait()
-}
-
-func startClient(i int) {
-	quit := make(chan struct{})
-	done := make(chan struct{})
-
-	c, err := net.Dial("tcp", ":8888")
+func startNewConn() {
+	conn, err := net.Dial("tcp", ":8888")
 	if err != nil {
-		panic(err)
+		log.Println("dial error:", err)
+		return
 	}
-	defer c.Close()
-	fmt.Println("client running...")
+	defer conn.Close()
+	log.Println("dial ok")
 
 	rng, err := codename.DefaultRNG()
 	if err != nil {
 		panic(err)
 	}
 
-	framePacker := frame.NewMyFramePacker()
+	frameCodec := frame.NewMyFramePacker()
+	var counter int
+
 	go func() {
 		for {
-			select {
-			case <-quit:
-				done <- struct{}{}
-				return
-			default:
-			}
-
-			c.SetReadDeadline(time.Now().Add(time.Second * 5))
-			ackFramePayload, err := framePacker.Unpack(c)
+			// handle ack
+			// read from the connection
+			ackFramePayLoad, err := frameCodec.Unpack(conn)
 			if err != nil {
-				if e, ok := err.(net.Error); ok {
-					if e.Timeout() {
-						continue
-					}
-				}
 				panic(err)
 			}
 
-			p, err := packet.Decode(ackFramePayload)
-			submitAck, ok := p.(*packet.SubmitAck)
-			if !ok {
-				panic("not submitAck")
-			}
+			p, err := packet.Decode(ackFramePayLoad)
 
-			fmt.Printf("[client %d]: the result of submit ack[%s] is %d\n", i, submitAck.ID, submitAck.Result)
+			_, ok := p.(*packet.SubmitAck)
+			if !ok {
+				panic("not submitack")
+			}
+			// fmt.Printf("the result of submit ack[%s] is %d\n", submitAck.ID, submitAck.Result)
 		}
 	}()
 
-	var counter int
 	for {
+		// send submit
 		counter++
-		id := fmt.Sprintf("%08d", counter)
-		packetPayload := codename.Generate(rng, 4)
+		id := fmt.Sprintf("%08d", counter) // 8 byte string
+		payload := codename.Generate(rng, 4)
 		s := &packet.Submit{
 			ID:      id,
-			Payload: []byte(packetPayload),
+			Payload: []byte(payload),
 		}
+		// fmt.Printf("send submit id = %s, payload=%s\n", s.ID, s.Payload)
+
 		framePayload, err := packet.Encode(s)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("[client %d]: send submit id = %s, payload=%s, frame length = %d\n",
-			i, s.ID, s.Payload, len(framePayload)+4)
 
-		err = framePacker.Pack(c, framePayload)
+		err = frameCodec.Pack(conn, framePayload)
 		if err != nil {
 			panic(err)
 		}
 
-		time.Sleep(1)
-		if counter >= 10 {
-			quit <- struct{}{}
-			<-done
-
-			fmt.Printf("client[%d] exiting...\n", i)
-			return
-		}
 	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			startNewConn()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
